9346a2d763b45bbc287fa30c0d1651d43eabddd8
larissa <larissa@debian-BULLSEYE-live-builder-AMD64>
2024-04-23 19:49:24 +0100

    First code working with variables.

---
+++ variables.js
@@ -0,0 +1,13 @@
+console.log("Working with variables");
+const idade = 26;
+console.log("Before :"+ idade);
+console.log(idade-2);
+console.log(idade+2);
+console.log(idade/2);
+console.log(idade**2);
+
+
+
+idadeSomada = idade + 3;
+console.log("idadeSomada: " + idadeSomada);
+
